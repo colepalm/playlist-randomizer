ecae77e280af9827c6cab0b57b1e23ce23e0a879
colepalm <palm.cole@gmail.com>
2020-12-20 13:17:25 -0700

    progress on oauth

--- wasm-ui/src/lib.rs
+++ wasm-ui/src/lib.rs
@@ -1,5 +1,9 @@
 use wasm_bindgen::prelude::*;
 
+let client_id = 'CLIENT_ID'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
