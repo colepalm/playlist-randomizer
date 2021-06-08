32a73fcc532bc6bbbe743ee3a2f5d47ef77fa463
colepalm <palm.cole@gmail.com>
2021-06-07 21:06:22 -0600

    refactor login into component + add router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
 import React, { Component } from "react";
-import "./App.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    // Link
+} from "react-router-dom";
 
-// Get the hash of the url
-const hash = window.location.hash
-    .substring(1)
-    .split("&")
-    .reduce(function(initial, item) {
-        if (item) {
-            var parts = item.split("=");
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-        }
-        return initial;
-    }, {});
-window.location.hash = "";
+import "./App.css";
+import { Login } from './components/Login/Login'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: null,
-        };
-    }
-
-    componentDidMount() {
-        // Set token
-        let _token = hash.access_token;
-
-        if (_token) {
-            // Set token
-            this.setState({
-                token: _token
-            });
-        }
-    }
-
-    componentWillUnmount() {
-        // clear the interval to save resources
-        clearInterval(this.interval);
-    }
-
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            {!this.state.token && (
-                <a
-                    className="btn btn--loginApp-link"
-                    href={`http://localhost:3000/login`}
-                >
-                  Login to Spotify
-                </a>
-            )}
-            {/*{this.state.token && (*/}
-            {/*    // Spotify Player Will Go Here In the Next Step*/}
-            {/*)}*/}
-          </header>
-        </div>
+        <Router>
+            <Switch>
+                <Route path='/login'>
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
     );
   }
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+import styles from './Login.module.css';
+
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+        };
+    }
+
+    render() {
+        return (
+            <div className={styles.Login}>
+                <div className="App">
+                    <header className="App-header">
+                        {!this.state.loggedIn && (
+                            <a className="btn btn--loginApp-link"
+                               href={`http://localhost:3000/login`}
+                            >
+                                Login to Spotify
+                            </a>
+                        )}
+                        {/*{this.state.token && (*/}
+                        {/*    // Spotify Player Will Go Here In the Next Step*/}
+                        {/*)}*/}
+                    </header>
+                </div>
+            </div>
+        )
+    }
+}
